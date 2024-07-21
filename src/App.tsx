aec0e4d49cb7f8005b5f6b1e5f1476df2e282509
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-07-21 13:50:51 +0200

    task2-KAN-30 adjust the folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Home from './components/Home';
 import Example1 from './components/Example1';
 import Example2 from './components/Example2';
 import Example3 from './components/Example3';
-
 import './App.css';
 
 const App: React.FC = () => {
