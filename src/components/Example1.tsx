ef44f3fc1fcad6bc8b583206ead1267c0a92f26c
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-07-22 13:04:01 +0200

    task3-KAN-6 create a table for example1 data

--- src/components/Example1.tsx
+++ src/components/Example1.tsx
@@ -2,8 +2,46 @@ import '../styles/example1.css';
 
 const Example1: React.FC = () => {
   return (
-    <div className="example">
-      <h1>Example1</h1>
+    <div className="container">
+      <div className="header_title">
+        <h2>Example 1</h2>
+      </div>
+      <div className="table_container">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Supplier</th>
+              <th>Certificate type</th>
+              <th>Valid from</th>
+              <th>Valid to</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td>Kellia AG, 1, Berlin</td>
+              <td>Permission of Printing</td>
+              <td>21.08.2017</td>
+              <td>26.08.2017</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Kellia GmbH, 1, Stuttgart</td>
+              <td>OHSAS 18001</td>
+              <td>18.08.2017</td>
+              <td>24.08.2017</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Kellia GmbH, 1, Stuttgart</td>
+              <td>Permission of Printing</td>
+              <td>04.10.2017</td>
+              <td>10.10.2017</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
