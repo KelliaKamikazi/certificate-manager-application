2560a7db59fa5dc437d4d36f5aaebd43d5927419
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-07-22 16:55:28 +0200

    task2-KAN-30 add scrollbar on the table

--- src/components/Example1.tsx
+++ src/components/Example1.tsx
@@ -3,11 +3,10 @@ import '../styles/example1.css';
 const Example1: React.FC = () => {
   return (
     <div className="container">
+      <div className="header_title">
+        <h2>Example 1</h2>
+      </div>
       <div className="table_container">
-        <div className="header_title">
-          <h2>Example 1</h2>
-        </div>
-
         <table>
           <thead>
             <tr>
@@ -28,17 +27,38 @@ const Example1: React.FC = () => {
             </tr>
             <tr>
               <td></td>
-              <td>Kellia GmbH, 1, Stuttgart</td>
-              <td>OHSAS 18001</td>
-              <td>18.08.2017</td>
-              <td>24.08.2017</td>
+              <td>Kellia AG, 1, Berlin</td>
+              <td>Permission of Printing</td>
+              <td>21.08.2017</td>
+              <td>26.08.2017</td>
             </tr>
             <tr>
               <td></td>
-              <td>Kellia GmbH, 1, Stuttgart</td>
+              <td>Kellia AG, 1, Berlin</td>
               <td>Permission of Printing</td>
-              <td>04.10.2017</td>
-              <td>10.10.2017</td>
+              <td>21.08.2017</td>
+              <td>26.08.2017</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Kellia AG, 1, Berlin</td>
+              <td>Permission of Printing</td>
+              <td>21.08.2017</td>
+              <td>26.08.2017</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Kellia AG, 1, Berlin</td>
+              <td>Permission of Printing</td>
+              <td>21.08.2017</td>
+              <td>26.08.2017</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Kellia AG, 1, Berlin</td>
+              <td>Permission of Printing</td>
+              <td>21.08.2017</td>
+              <td>26.08.2017</td>
             </tr>
           </tbody>
         </table>
