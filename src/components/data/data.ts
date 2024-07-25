4790d587a566fd6f458553b001f76f2e0faa67c5
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-07-25 10:37:06 +0200

    task4-KAN-56- Add an datasampleInterface

--- src/components/data/data.ts
+++ src/components/data/data.ts
@@ -5,19 +5,4 @@ export interface Certificate {
   validFrom: Date;
   validTo: Date;
 }
-
-// Sample data initialization
-export const sampleCertificates: Certificate[] = [
-  /*{
-    supplier: 'Kellia AG, 1, Berlin',
-    certificateType: 'Permission of Printing',
-    validFrom: new Date('2017-08-21'),
-    validTo: new Date('2017-08-21'),
-  },
-  {
-    supplier: 'Kamikazi AG, 1, Berlin',
-    certificateType: 'Permission of Printing',
-    validFrom: new Date('2017-08-21'),
-    validTo: new Date('2017-08-21'),
-  },*/
-];
+export const sampleCertificates: Certificate[] = [];
