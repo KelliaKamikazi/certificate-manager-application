cdbf06a4f9b11c112b6496a952ed72c546dcd994
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-07-26 11:05:24 +0200

    task5-KAN-69-Add the certificate page

--- src/components/Example1.tsx
+++ src/components/Example1.tsx
@@ -1,22 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/example1.css';
-import { getData } from '../utils/indexedDB';
 import { Certificate } from './data/data';
 import { Link } from 'react-router-dom';
 import IconSvg from './icons/icons';
 import gearIcon from './icons/gearIcon';
+import { getData, deleteData } from '../utils/indexedDB';
 
 const Example1: React.FC = () => {
   const [certificates, setCertificates] = useState<Certificate[]>([]);
-
   useEffect(() => {
     const fetchData = async () => {
       const data = await getData();
       setCertificates(data);
     };
-
     fetchData().catch(console.error);
   }, []);
+  //For the dropdown
+  const [openDropdownId, setOpenDropdownId] = useState<number | undefined>(
+    undefined,
+  );
+  const toggleDropdown = (certId: number | undefined) => {
+    setOpenDropdownId(openDropdownId === certId ? undefined : certId);
+  };
+
+  const confirmAndDelete = async (id: number) => {
+    if (window.confirm('Are you sure you want to delete this certificate?')) {
+      try {
+        await deleteData(id);
+        alert('Certificate was deleted successfully');
+        setCertificates((prevCertificates) =>
+          prevCertificates.filter((cert) => cert.id !== id),
+        );
+      } catch (error) {
+        console.error('Failed to delete the certificate', error);
+        alert('Certificate not deleted');
+      }
+    }
+  };
+
+  const handleDeleteClick = (id: number | undefined) => {
+    if (id !== undefined) {
+      confirmAndDelete(id);
+    } else {
+      alert('Certificate ID is undefined');
+    }
+  };
 
   return (
     <div className="container">
@@ -28,9 +56,7 @@ const Example1: React.FC = () => {
         <table>
           <thead>
             <tr>
-              <th className="gearIcon">
-                <IconSvg Icon={gearIcon} />
-              </th>
+              <th></th>
               <th>Supplier</th>
               <th>Certificate type</th>
               <th>Valid from</th>
@@ -40,7 +66,33 @@ const Example1: React.FC = () => {
           <tbody>
             {certificates.map((cert) => (
               <tr key={cert.id}>
-                <td></td>
+                <td>
+                  <div className="dropdown-container">
+                    <IconSvg
+                      Icon={gearIcon}
+                      className="gear-icon cursor-pointer"
+                      onClick={() => toggleDropdown(cert.id)}
+                    />
+                    {openDropdownId === cert.id && (
+                      <div className="dropdown-menu">
+                        <div className="dropdown-options">
+                          <button
+                            className="dropdown-button"
+                            onClick={() => {}}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="dropdown-button"
+                            onClick={() => handleDeleteClick(cert.id)}
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </td>
                 <td>{cert.supplier}</td>
                 <td>{cert.certificateType}</td>
                 <td>{cert.validFrom.toDateString()}</td>
@@ -53,5 +105,4 @@ const Example1: React.FC = () => {
     </div>
   );
 };
-
 export default Example1;
---
+++ src/components/data/certificateService.ts
@@ -0,0 +1,19 @@
+// import { Certificate } from './data';
+// import { getData } from '../utils/indexedDB';
+
+// const saveCertificate = async (certificate: Certificate): Promise<void> => {
+//   try {
+//     await addData([certificate]);
+//     alert('Certificate was saved successfully');
+//   } catch (error) {
+//     console.error('Failed to add the certificate', error);
+//     alert('Certificate not added');
+//   }
+// };
+// const displayData = async (certificate : Certificate): Promise<void> =>{
+//     try{
+//         await getData
+//     }
+// }
+
+// export { saveCertificate };
