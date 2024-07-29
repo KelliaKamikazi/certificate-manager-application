0ba546eae3c7e6849cfbc7006b4d5f8de6e92163
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-07-29 06:57:33 +0200

    task7-KAN-71 Hard code suppliers

--- src/components/views/Header.tsx
+++ src/components/views/Header.tsx
@@ -1,9 +1,36 @@
+import React, { useState } from 'react';
 import '../../styles/header.css';
 
 const Header: React.FC = () => {
+  const [language, setLanguage] = useState('English');
+
+  const changeLanguage = (lang: string) => {
+    setLanguage(lang);
+  };
+
   return (
     <header className="title">
       <h1>DCCS Tuzla</h1>
+      <div className="lang-dropdown-container">
+        <span>Language:</span>
+        <div className="languages">
+          <button className="lang-button">{language} ▼</button>
+          <div className="dropdown-content">
+            <a
+              href="#"
+              onClick={() => changeLanguage('English')}
+            >
+              English
+            </a>
+            <a
+              href="#"
+              onClick={() => changeLanguage('German')}
+            >
+              German
+            </a>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
