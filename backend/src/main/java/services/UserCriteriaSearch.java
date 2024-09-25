5dd41d317fd69296c5e447327021bb9ebec8b35c
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-09-25 13:04:21 +0200

    Add search functionality for users

---
+++ backend/src/main/java/services/UserCriteriaSearch.java
@@ -0,0 +1,50 @@
+package services;
+
+
+import data.entities.UserEntity;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class UserCriteriaSearch {
+
+    private final EntityManager entityManager;
+    @Inject
+    public UserCriteriaSearch(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    public List<UserEntity>searchUsers(String userId, String firstName, String lastName,String email,String plant){
+
+        CriteriaBuilder cb= entityManager.getCriteriaBuilder();
+        CriteriaQuery<UserEntity> query= cb.createQuery(UserEntity.class);
+        Root<UserEntity>root =query.from(UserEntity.class);
+        List<Predicate>predicates= new ArrayList<>();
+
+        if (userId != null && !userId.isEmpty()) {
+            predicates.add(cb.equal(root.get("userId"), userId));
+        }
+        if (firstName != null && !firstName.isEmpty()) {
+            predicates.add(cb.like(cb.lower(root.get("firstName")), "%" + firstName.toLowerCase() + "%"));
+        }
+        if (lastName != null && !lastName.isEmpty()) {
+            predicates.add(cb.like(cb.lower(root.get("lastName")), "%" + lastName.toLowerCase() + "%"));
+        }
+        if (email != null && !email.isEmpty()) {
+            predicates.add(cb.like(cb.lower(root.get("email")), "%" + email.toLowerCase() + "%"));
+        }
+        if (plant != null && !plant.isEmpty()) {
+            predicates.add(cb.equal(root.get("plant"), plant));
+        }
+        query.where(predicates.toArray(new Predicate[0]));
+
+        return entityManager.createQuery(query).getResultList();
+    }
+}
--- backend/src/main/java/web/mappers/UserMapper.java
+++ backend/src/main/java/web/mappers/UserMapper.java
@@ -1,4 +1,18 @@
 package web.mappers;
 
+import data.entities.UserEntity;
+import jakarta.enterprise.context.ApplicationScoped;
+import web.dtos.UserDto;
+
+@ApplicationScoped
 public class UserMapper {
+    public UserDto toDTO(UserEntity entity) {
+        UserDto dto = new UserDto();
+        dto.setUserId(entity.getUserId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setEmail(entity.getEmail());
+        dto.setPlant(entity.getPlant());
+        return dto;
+    }
 }
