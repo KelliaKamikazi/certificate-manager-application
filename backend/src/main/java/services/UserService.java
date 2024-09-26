0bd0496740e5873f2736816d9c72243cb02e9469
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-09-26 13:22:48 +0200

    search

--- backend/src/main/java/services/UserService.java
+++ backend/src/main/java/services/UserService.java
@@ -1,24 +1,17 @@
 package services;
 
-import data.entities.UserEntity;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import web.dtos.UserDto;
-import web.mappers.UserMapper;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class UserService {
     @Inject
     private UserCriteriaSearch userCriteriaSearch;
 
-    @Inject
-    private UserMapper userMapper;
-
-    public List<UserDto> searchUsers(String userId, String firstName,String lastName, String email, String department,String plant) {
-        List<UserEntity> entities = userCriteriaSearch.searchUsers(userId, firstName,lastName, email,department, plant);
-        return entities.stream().map(userMapper::toDTO).collect(Collectors.toList());
+    public List<UserDto> searchUsers(String userId, String firstName, String lastName, String email, String department, String plant) {
+        return userCriteriaSearch.searchUsers(userId, firstName, lastName, email, department, plant);
     }
 }
