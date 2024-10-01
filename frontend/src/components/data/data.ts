c7ba87f0ae28bb19dc2ceb9c8424bcb5cb82b736
Kamikazi Kellia <115183307+KelliaKamikazi@users.noreply.github.com>
2024-10-01 12:03:49 +0200

    remove the before used supplier and certificates

--- frontend/src/components/data/data.ts
+++ frontend/src/components/data/data.ts
@@ -1,29 +1,3 @@
-export interface Certificate {
-  id: number;
-  supplier: Supplier;
-  certificateType: string;
-  validFrom: Date;
-  validTo: Date;
-  pdfUrl?: string;
-}
-export interface Supplier {
-  name: string;
-  supplierIndex?: number | undefined;
-  city?: string;
-}
-export const INITIAL_CERTIFICATE = {
-  supplier: { name: "" },
-  certificateType: "",
-  validTo: "",
-  validFrom: "",
-  pdfUrl: undefined as string | undefined,
-};
-export const sampleCertificates: Certificate[] = [];
-
-export enum Certificate_Type {
-  PERMISSION_OF_PRINTING = "Permission of Printing",
-  CCC_CERTIFICATE = "CCC Certificate",
-}
 export interface Participant {
   id?: number;
   userId: string;
